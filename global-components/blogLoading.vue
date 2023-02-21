468231ca1be6a69d7aadd63e57f62bb1d6398de8
eddievandeer <1255173159@qq.com>
2023-02-21 21:02:13 +0800

    feat: show loading when data is not ready'

---
+++ global-components/blogLoading.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="blog-loading">
+    <i class="fa fa-spinner fa-pulse fa-5x" aria-hidden="true"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "blogLoading",
+};
+</script>
+
+<style lang="scss" scoped>
+.blog-loading {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #A2A2A3;
+  background-color: #f3f3f4;
+}
+</style>
--- layouts/Layout.vue
+++ layouts/Layout.vue
@@ -1,54 +1,57 @@
 <template>
   <div :class="['theme-container', {'sidebar': sidebar}, {'list': list}]">
-    <blog-header v-on:toggle="handleList"></blog-header>
-    <title-list :sidebar="list"></title-list>
-    <blogSidebar :sidebar="sidebar"></blogSidebar>
+    <blog-loading v-if="$site.pages.length === 0" />
+    <div v-else>
+      <blog-header v-on:toggle="handleList"></blog-header>
+      <title-list :sidebar="list"></title-list>
+      <blogSidebar :sidebar="sidebar"></blogSidebar>
 
-    <div class="theme-container__inner">
-      <!-- 主页 -->
-      <section class="main-container">
-        <blog-home v-if="$page.frontmatter.home"></blog-home>
+      <div class="theme-container__inner">
+        <!-- 主页 -->
+        <section class="main-container">
+          <blog-home v-if="$page.frontmatter.home"></blog-home>
 
-        <!-- 列表页 -->
-        <blog-list
-          v-else-if="$page.frontmatter.list"
-          :pageNumber="$route.params.pageNumber"
-        ></blog-list>
+          <!-- 列表页 -->
+          <blog-list
+            v-else-if="$page.frontmatter.list"
+            :pageNumber="$route.params.pageNumber"
+          ></blog-list>
 
-        <!-- 分类页 -->
-        <blog-categories v-else-if="$page.frontmatter.categoriesPage"></blog-categories>
+          <!-- 分类页 -->
+          <blog-categories v-else-if="$page.frontmatter.categoriesPage"></blog-categories>
 
-        <!-- 标签页 -->
-        <blog-tags v-else-if="$page.frontmatter.tagsPage"></blog-tags>
+          <!-- 标签页 -->
+          <blog-tags v-else-if="$page.frontmatter.tagsPage"></blog-tags>
 
-        <!-- 关于页 -->
-        <about-me v-else-if="$page.frontmatter.aboutme"></about-me>
+          <!-- 关于页 -->
+          <about-me v-else-if="$page.frontmatter.aboutme"></about-me>
 
-        <!-- 特殊页 -->
-        <div v-else-if="specialPage()">
-          <blog-category
-            v-if="$route.params.category"
-            :category="$route.params.category"
-            :child="$route.params.child"
-            :pageNumber="$route.params.pageNumber || '1'"
-          ></blog-category>
-          <blog-tag
-            v-else-if="$route.params.tag"
-            :tag="$route.params.tag"
-            :pageNumber="$route.params.pageNumber || '1'"
-          ></blog-tag>
-          <blog-articles
-            v-else
-            :pageNumber="$route.params.pageNumber || '1'"
-            :filted="$listPages"
-            :path="`${this.$route.path.split('/')[1]}${this.$route.path.split('/')[1] !== 'page' ? '/' + this.$route.path.split('/')[2] : ''}`"
-          ></blog-articles>
-        </div>
+          <!-- 特殊页 -->
+          <div v-else-if="specialPage()">
+            <blog-category
+              v-if="$route.params.category"
+              :category="$route.params.category"
+              :child="$route.params.child"
+              :pageNumber="$route.params.pageNumber || '1'"
+            ></blog-category>
+            <blog-tag
+              v-else-if="$route.params.tag"
+              :tag="$route.params.tag"
+              :pageNumber="$route.params.pageNumber || '1'"
+            ></blog-tag>
+            <blog-articles
+              v-else
+              :pageNumber="$route.params.pageNumber || '1'"
+              :filted="$listPages"
+              :path="`${this.$route.path.split('/')[1]}${this.$route.path.split('/')[1] !== 'page' ? '/' + this.$route.path.split('/')[2] : ''}`"
+            ></blog-articles>
+          </div>
 
-        <!-- 文章页 -->
-        <blog-page v-else></blog-page>
-        <blog-footer v-if="!$page.frontmatter.home"></blog-footer>
-      </section>
+          <!-- 文章页 -->
+          <blog-page v-else></blog-page>
+          <blog-footer v-if="!$page.frontmatter.home"></blog-footer>
+        </section>
+      </div>
     </div>
   </div>
 </template>
